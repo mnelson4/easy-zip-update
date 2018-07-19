d0e256c145027b71202e92346e6e208dfea382fb
Mike Nelson <michael@eventespresso.com>
2018-07-19 09:39:34 -0700

    bump version

--- easy-zip-update.php
+++ easy-zip-update.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Easy Zip Update
-Plugin URI: http://wordpress.org/
-Description: Simplifies updating plugins and themes using a zip file. Once this plugin is active, to update a plugin, go to "Plugins", then "Add New", then "Upload Plugin". The uploaded plugin will be updated, no need to deactivate it, delete it, and reactivate it.
+Plugin URI: https://cmljnelson.wordpress.com
+Description: Simplifies updating plugins and themes using a zip file. Once this plugin is active, to update a plugin, go to "Plugins", then "Add New", then "Upload Plugin". The uploaded plugin will be updated, no need to deactivate it, delete it, and reactivate it. Please note: this is BETA software. Use at your own risk.
 Author: Mike Nelson
-Version: 0.1
+Version: 0.1.1
 Author URI: https://cmljnelson.wordpress.com
 */
 function ezu_replace( $options )
